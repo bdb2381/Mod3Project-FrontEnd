ded22db2b565f1453f98d1af4b0705657e1a12c3
bdb2381 <brad.beggs@gmail.com>
2020-09-08 13:03:21 -0700

    invoked the rendering of the map and api key in .gitignore

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
-let MapsKey = config.GOOGLE_KEY;
+const MapsKey = config.GOOGLE_KEY;
 // config relates to the config{} in configAPI.js
+
+loadApiSource()
+renderMap()
+
+
+function loadApiSource(){
+  const apiKey = document.querySelector('head')
+  const newTag = document.createElement('script')
+  
+  newTag.src = `https://maps.googleapis.com/maps/api/js?key=${MapsKey}&callback=initMap`
+  
+  apiKey.append(newTag)
+}
+
+
+function renderMap(){
+  const mapElement = document.getElementById('map')
+  const mapScriptTag = document.createElement('script')
+  
+  mapScriptTag.text = `            
+  let map;
+
+  function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8
+    });
+  }
+    `
+  
+    mapElement.append(mapScriptTag)   
+}
