3b0c695ec75e408d44c15cbb90efdc5caf94bbec
bbtrombone <bbtrombone@sbcglobal.net>
2020-09-10 16:36:37 -0700

    weird

--- src/index.js
+++ src/index.js
@@ -13,30 +13,6 @@
 
 
 
-<<<<<<< HEAD
-=======
-function displayNotes(element){
-    if(element.User_id == parseInt(parseInt(sessionStorage.getItem(setWord)))){
-        let newLi = document.createElement("li")
-        newLi.innerText = element.text
-        getList.appendChild(newLi)
-      
-        deleteButton = document.createElement("button")
-        deleteButton.innerText = "delete"
- 
-        divElement = document.createElement('div') //
-        divElement.appendChild(deleteButton) //
-        newLi.append(divElement) //
-        newLi.id = element.id
-        deleteButton.addEventListener("click", deleteNote) //
-               
-        editButton = document.createElement("button")
-        divElement.appendChild(editButton) //
-        editButton.innerText ="edit"
-        editButton.className = element.id
-        // debugger
-    }
->>>>>>> 4fbc7956dab2e92b2cfbd61f66266277906693db
 
 
 //////////////////
@@ -47,7 +23,7 @@ function displayNotes(element){
 
 
 //////////////////
-// Manipulate the DOM Functions
+// Manipulate the DOM Functionsgut 
 
       
 
@@ -109,29 +85,8 @@ function getNotes(){
 }
 getNotes()
 
-<<<<<<< HEAD
 function patchNote(event){
     event.preventDefault()
-=======
-function postNote(event){
-    event.preventDefault()
-    
-    //  debugger  
-    if( setWord && setPlace != ""){
-        let data ={ 
-            text: event.target[0].value,
-            User_id: parseInt(sessionStorage.getItem(setWord)),
-            id: parseInt(sessionStorage.getItem(setPlace))
-        }
-        fetch(`http://localhost:3000/notes`,{
-        method: "POST",
-        headers: {'Content-Type':'application/json'},
-        body: JSON.stringify(data)
-        })
-        // debugger
-        getNotes(data)
-    }
->>>>>>> 4fbc7956dab2e92b2cfbd61f66266277906693db
     // debugger
     let data ={text: event.target[0].value,
                Park_id: sessionStorage.getItem("testpark"),
